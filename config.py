b62b1cc812442bbc885d625960932c40bd768f88
blankmeta <yadanilfilatov@gmail.com>
2022-10-22 14:54:40 +0300

    Files uploading

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 from pprint import pprint
 
-BUTTONS_PER_STRING = 5
-BUTTONS_PER_COLUMN = 5
+BUTTONS_PER_STRING = 30
+BUTTONS_PER_COLUMN = 30
 
+WINDOW_HEIGHT = 800
+WINDOW_WIDTH = 800
 
+DELAY = 0.25
 
 if __name__ == '__main__':
     array = [[str_bt for str_bt in range(buttons_per_string)]
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+import pickle
 from pprint import pprint
-
-import pygame
+from time import sleep
 from typing import List
 
-from config import BUTTONS_PER_STRING, BUTTONS_PER_COLUMN
+import pygame
+
+from config import (BUTTONS_PER_STRING, BUTTONS_PER_COLUMN, WINDOW_HEIGHT,
+                    WINDOW_WIDTH, DELAY)
 
 pygame.init()
-screen = pygame.display.set_mode((750, 750))
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 screen.fill('white')
 clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial", 20)
+font = pygame.font.SysFont("Arial", WINDOW_WIDTH // BUTTONS_PER_STRING)
 
 colors = ['grey', 'black', 'red', 'green']
 
@@ -26,7 +29,8 @@ class Button:
 
     def __init__(self, pos, bg='black', feedback=''):
         self.x, self.y = pos
-        self.size = (100, 100)
+        self.size = (WINDOW_WIDTH / BUTTONS_PER_STRING - 1,
+                     WINDOW_HEIGHT / BUTTONS_PER_COLUMN - 1)
         self.font = font
         self.surface = pygame.Surface(self.size)
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
@@ -34,17 +38,23 @@ class Button:
         self.surface.fill(bg)
 
         if feedback == "":
-            self.feedback = "text"
+            self.feedback = 'text'
         else:
             self.feedback = feedback
 
-    def change_status(self, bg="black"):
+    def change_status(self, bg='black'):
         """Change the color what you click"""
         self.bg = bg
         self.surface.fill(bg)
 
     def change_text(self, text: str) -> None:
-        text = self.font.render(text, True, pygame.Color("White"))
+        if text == 'W':
+            self.change_status(bg='purple')
+
+        self.surface.blit(self.font.render('', False, pygame.Color("White")),
+                          (0, 0))
+
+        text = self.font.render(text, False, pygame.Color("White"))
         self.surface.blit(text, (0, 0))
 
     def show(self):
@@ -122,6 +132,65 @@ def build_lee(array: List[list], dot_coords: List[int]):
     return array
 
 
+def path_recovery(array: List[List[str]]):
+    dot_coords = []
+    for line_count, line in enumerate(array):
+        for value_count, value in enumerate(line):
+            print(value)
+            if value == 'f':
+                dot_coords = [line_count, value_count]
+
+    values_around = []
+    coords_around = []
+    print(f'Recovery {dot_coords}')
+    min_value = 0
+
+    while min_value != 1:
+        # up
+        if (dot_coords[0] - 1) >= 0:
+            value = array[dot_coords[0] - 1][dot_coords[1]]
+            if isinstance(value, int):
+                values_around.append(value)
+                coords_around.append([dot_coords[0] - 1, dot_coords[1]])
+
+        # right
+        if (dot_coords[1] + 1) < BUTTONS_PER_STRING:
+            value = array[dot_coords[0]][dot_coords[1] + 1]
+            if isinstance(value, int):
+                values_around.append(value)
+                coords_around.append([dot_coords[0], dot_coords[1] + 1])
+
+        # left
+        if (dot_coords[1] - 1) >= 0:
+            value = array[dot_coords[0]][dot_coords[1] - 1]
+            if isinstance(value, int):
+                values_around.append(value)
+                coords_around.append([dot_coords[0], dot_coords[1] - 1])
+
+        # down
+        if (dot_coords[0] + 1) < BUTTONS_PER_COLUMN:
+            value = array[dot_coords[0] + 1][dot_coords[1]]
+            if isinstance(value, int):
+                values_around.append(value)
+                coords_around.append([dot_coords[0] + 1, dot_coords[1]])
+
+        min_value = min(values_around)
+
+        dot_coords = coords_around[values_around.index(min_value)]
+        print(min_value)
+        array[dot_coords[0]][dot_coords[1]] = 'W'
+
+        sleep(DELAY)
+        array_to_buttons(array, buttons)
+        for st in buttons:  # returns list
+            for button in st:
+                button.show()
+        clock.tick(30)
+        pygame.display.update()
+
+    pprint(array)
+
+
 def lee(array: List[list]) -> List[list]:
     start_coords = []
     status = 0
@@ -129,9 +198,11 @@ def lee(array: List[list]) -> List[list]:
         for char_count, char in enumerate(line):
             if char == 's':
                 start_coords = [line_count, char_count]
-    for i in range(8):
+    status_coords = start_coords
+    while len(status_coords) > 0:
         status_coords = []
         if status == 0:
+            status_coords = start_coords
             array = build_lee(array, start_coords)
             # pprint(array)
         else:
@@ -143,12 +214,31 @@ def lee(array: List[list]) -> List[list]:
                 array = build_lee(array, coords)
                 # pprint(array)
         status += 1
+        sleep(DELAY)
+        print(len(status_coords))
+        array_to_buttons(array, buttons)
+        for st in buttons:  # returns list
+            for button in st:
+                button.show()
+        clock.tick(30)
+        pygame.display.update()
     return array
+
+
+def to_buttons(array: List[list], buttons: List[List[Button]]) -> None:
+    for line_count, line in enumerate(array):
+        for values_count, value in enumerate(line):
+            buttons[line_count][values_count].change_status(
+                list(color_to_char.keys())[
+                    list(color_to_char.values()).index(value)]
+            )
 
 
 def mainloop():
     """ The infinite loop where things happen """
     lee_status = 0
+    array = buttons_to_array(buttons)
+    array = lee(array)
 
     while True:
         for event in pygame.event.get():
@@ -163,9 +253,24 @@ def mainloop():
                         pprint(array)
                         array = lee(array)
                         pprint(array)
-                        array_to_buttons(array, buttons)
+                        # array_to_buttons(array, buttons)
+                    if event.key == pygame.K_F1:
+                        with open('scheme.pickle', 'wb') as f:
+                            pickle.dump(buttons_to_array(buttons), f)
+                        print('Saved')
+                    if event.key == pygame.K_F2:
+                        with open('scheme.pickle', 'rb') as f:
+                            array = pickle.load(f)
+                            clean(buttons)
+                            to_buttons(array, buttons)
                 except IndexError:
                     print('Fill the space')
+                try:
+                    if event.key == pygame.K_DOWN:
+                        path_recovery(array)
+                        # array_to_buttons(array, buttons)
+                except ValueError:
+                    print('Impossible')
             for st in buttons:  # returns list
                 for button in st:
                     button.click(event)
@@ -183,7 +288,8 @@ def console_info():
 
 buttons = [[Button(
     # (str_bt * 101, cl_bt * 101),
-    (str_bt * 101, cl_bt * 101),
+    (str_bt * WINDOW_WIDTH / BUTTONS_PER_STRING + str_bt,
+     cl_bt * WINDOW_HEIGHT / BUTTONS_PER_COLUMN + cl_bt),
     bg=colors[0],
     feedback="You clicked me")
     for str_bt in range(BUTTONS_PER_STRING)]
